250e74caf3baa1839859bd54ee299c5d5ee2e546
cdpidan <jks_pidan@foxmail.com>
2019-10-12 13:34:34 +0800

    chenge shell

--- docker/shadowsocks-libev/docker.sh
+++ docker/shadowsocks-libev/docker.sh
@@ -1,14 +1,38 @@
-yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum makecache fast
-yum install docker-ce -y
-systemctl start docker
+# Firewall set
+firewall_set(){
+    echo -e "[${green}Info${plain}] firewall set start..."
+    systemctl status firewalld > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        default_zone=$(firewall-cmd --get-default-zone)
+        firewall-cmd --permanent --zone=${default_zone} --add-port=${shadowsocksport}/tcp
+        firewall-cmd --permanent --zone=${default_zone} --add-port=${shadowsocksport}/udp
+        firewall-cmd --reload
+    else
+        echo -e "[${yellow}Warning${plain}] firewalld looks like not running or not installed, please enable port ${shadowsocksport} manually if necessary."
+    fi
+    echo -e "[${green}Info${plain}] firewall set completed..."
+}
 
-mkdir -p /etc/shadowsocks-libev
-cat > /etc/shadowsocks-libev/config.json<<-EOF
+install_docker(){
+    docker version > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Docker-ce exists ..."
+    else
+        yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+        yum makecache fast
+        yum install --nobest docker-ce -y
+        systemctl enable docker
+        systemctl start docker
+    fi
+}
+
+run_docker(){
+    mkdir -p /etc/shadowsocks-libev
+	cat > /etc/shadowsocks-libev/config.json<<-EOF
 {
 	"server": "0.0.0.0",
-	"server_port": $1,
-	"password": "$2",
+	"server_port": ${shadowsocksport},
+	"password": "${shadowsockspwd}",
 	"timeout": 300,
 	"user": "nobody",
 	"method": "chacha20-ietf-poly1305",
@@ -17,5 +41,26 @@ cat > /etc/shadowsocks-libev/config.json<<-EOF
 	"mode": "tcp_and_udp"
 }
 EOF
+    
+	docker rm -f ss-libev > /dev/null 2>&1
+    docker run -d -p ${shadowsocksport}:${shadowsocksport} -p ${shadowsocksport}:${shadowsocksport}/udp --restart=always --name ss-libev -v /etc/shadowsocks-libev:/etc/shadowsocks-libev teddysun/shadowsocks-libev
+}
 
-docker run -d -p $1:$1 -p $1:$1/udp --restart=always --name ss-libev -v /etc/shadowsocks-libev:/etc/shadowsocks-libev teddysun/shadowsocks-libev
+format_parameters(){
+    [ -z $shadowsocksport ] && shadowsocksport=21321
+    [ -z $shadowsockspwd ] && shadowsockspwd="helloworld123"
+}
+
+install_shadowsocks_libev(){
+    install_docker
+    format_parameters
+    run_docker
+    firewall_set
+}
+
+# Initialization step
+
+shadowsocksport=$1
+shadowsockspwd=$2
+
+install_shadowsocks_libev
